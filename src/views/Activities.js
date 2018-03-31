94c9b88367550b2a9564bfb9fa7d500dfc6b7b4d
Jerome Burkard <jerome.burkard@gadz.org>
2018-03-31 18:01:52 -0300

    Adding eslinc

---
+++ src/views/Activities.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Activities extends React.Component {
+  static navigationOptions = {
+    title: 'Activity page'
+  }
+
+  render() {
+    return <div>Activities page</div>
+  }
+}
+
+export default Activities
---
+++ src/views/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Login extends React.Component {
+  static navigationOptions = {
+    title: 'Login page'
+  }
+
+  render() {
+    return <div>Login page</div>
+  }
+}
+
+export default Login
